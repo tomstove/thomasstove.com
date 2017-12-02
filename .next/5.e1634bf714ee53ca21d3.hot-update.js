1ec97f5b421c0c93c4ee8437c5f3a348a37d5713
Tom Stove <thomstove@gmail.com>
2017-12-02 14:55:36 +1100

    Made responsive for mobile max-width: 450px

---
+++ .next/5.e1634bf714ee53ca21d3.hot-update.js
@@ -0,0 +1,162 @@
+webpackHotUpdate(5,{
+
+/***/ 101:
+false,
+
+/***/ 102:
+false,
+
+/***/ 105:
+false,
+
+/***/ 106:
+false,
+
+/***/ 107:
+false,
+
+/***/ 108:
+false,
+
+/***/ 128:
+false,
+
+/***/ 129:
+false,
+
+/***/ 134:
+false,
+
+/***/ 135:
+false,
+
+/***/ 136:
+false,
+
+/***/ 137:
+false,
+
+/***/ 138:
+false,
+
+/***/ 139:
+false,
+
+/***/ 140:
+false,
+
+/***/ 141:
+false,
+
+/***/ 142:
+false,
+
+/***/ 155:
+false,
+
+/***/ 156:
+false,
+
+/***/ 157:
+false,
+
+/***/ 158:
+false,
+
+/***/ 162:
+false,
+
+/***/ 163:
+false,
+
+/***/ 164:
+false,
+
+/***/ 165:
+false,
+
+/***/ 166:
+false,
+
+/***/ 175:
+false,
+
+/***/ 176:
+false,
+
+/***/ 177:
+false,
+
+/***/ 178:
+false,
+
+/***/ 179:
+false,
+
+/***/ 181:
+false,
+
+/***/ 189:
+false,
+
+/***/ 190:
+false,
+
+/***/ 194:
+false,
+
+/***/ 232:
+false,
+
+/***/ 233:
+false,
+
+/***/ 234:
+false,
+
+/***/ 235:
+false,
+
+/***/ 236:
+false,
+
+/***/ 237:
+false,
+
+/***/ 238:
+false,
+
+/***/ 398:
+/***/ (function(module, exports) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: Expected corresponding JSX closing tag for <meta> (18:4)\n\n\u001b[0m \u001b[90m 16 | \u001b[39m      \u001b[33m<\u001b[39m\u001b[33mlink\u001b[39m rel\u001b[33m=\u001b[39m\u001b[32m\"stylesheet\"\u001b[39m href\u001b[33m=\u001b[39m\u001b[32m\"https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css\"\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 17 | \u001b[39m      \u001b[33m<\u001b[39m\u001b[33mlink\u001b[39m rel\u001b[33m=\u001b[39m\u001b[32m\"stylesheet\"\u001b[39m href\u001b[33m=\u001b[39m\u001b[32m\"/static/style.css\"\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 18 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mHead\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m    | \u001b[39m    \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 19 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 20 | \u001b[39m)\n \u001b[90m 21 | \u001b[39m\u001b[0m\n");
+
+/***/ }),
+
+/***/ 399:
+false,
+
+/***/ 52:
+false,
+
+/***/ 71:
+false,
+
+/***/ 76:
+false,
+
+/***/ 77:
+false,
+
+/***/ 80:
+false,
+
+/***/ 82:
+false,
+
+/***/ 83:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNS5lMTYzNGJmNzE0ZWU1M2NhMjFkMy5ob3QtdXBkYXRlLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0EiLCJzb3VyY2VSb290IjoiIn0=
